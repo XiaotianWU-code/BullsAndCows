204bdeea364eb15079113b08298426185eccd019
Xiaotian WU <XWu32@slb.com>
2022-10-28 14:51:52 +0800

    test: Should return 2A0B when two same

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,17 +5,30 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret;
 
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            return "4A0B";
+            var guessList = guess.Split(" ");
+            var secretList = secret.Split(" ");
+            var cntA = 0;
+            for (var i = 0; i < secretList.Length; i++)
+            {
+                if (guessList[i] == secretList[i])
+                {
+                    cntA++;
+                }
+            }
+
+            return $"{cntA}A0B";
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -27,5 +27,18 @@ namespace BullsAndCowsTest
             // then
             Assert.Equal("4A0B", guessResult);
         }
+
+        [Fact]
+        public void Should_return_2A0B_when_guess_given_the_guess_two_digits_as_same_as_the_secret()
+        {
+            // given
+            var mockSecretGeneratro = new Mock<SecretGenerator>();
+            mockSecretGeneratro.Setup(x => x.GenerateSecret()).Returns("1 2 3 4");
+            var game = new BullsAndCowsGame(mockSecretGeneratro.Object);
+            // when
+            var guessResult = game.Guess("1 2 5 6");
+            // then
+            Assert.Equal("2A0B", guessResult);
+        }
     }
 }
